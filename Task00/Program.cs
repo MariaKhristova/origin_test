a81cf2388a2e41bc014aee552d8f5508315a33dd
Maria <masha-minsk@mail.ru>
2023-08-17 14:30:53 +0300

    add new tasks

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,3 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write ("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int square = num * num;
+Console.Write ($"Квадрат числа {num} = {square}");
+
 
